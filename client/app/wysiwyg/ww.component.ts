4cae06de03cf4a537488133732f476146425003b
DESKTOP-0NGOKFC\iddms <iddms5446@gmail.com>
2017-08-23 14:35:14 +0900

    SafeHtml 파이프를 만들어 innerHTML에 SafeHtml로 변환해 넣어주면 style도 잘 보인다.

--- client/app/wysiwyg/ww.component.ts
+++ client/app/wysiwyg/ww.component.ts
@@ -9,14 +9,21 @@ export class WwComponent{
     @ViewChild('editor') editor : ElementRef;
 
     htmlContent : string;
+    isHtml : boolean;
 
     display() : void {
         this.htmlContent = this.editor.nativeElement.innerHTML;
     }
-    event_change(msg : string) : void {
-        console.log("event : " + msg);
+    event_keydown(event : any) : void {
+        //console.log("event : " + msg);
+        if(event.keyCode === 9){
+            document.execCommand('insertHTML', false, '&#009');
+            // 다음 엘리먼트로 포커스 옮기는 거 방지
+            event.preventDefault();
+        }
     }
     command(cmd : string, value : any){
+        console.log(cmd + ", " + value);
         switch(cmd){
         case "bold":
             document.execCommand('bold', false, null);
@@ -27,10 +34,42 @@ export class WwComponent{
         case "italic" :
             document.execCommand('italic', false, null);
             break;
-        case 'InsertInputCheckbox':
-            document.execCommand('InsertInputCheckbox', false, null);
+        case 'insertUnorderedList':
+            document.execCommand('insertUnorderedList', false, null);
+            break;
+        case 'formatBlock':
+            document.execCommand('formatBlock', false, value);
+            break;
+        case 'fontSize':
+            document.execCommand('fontSize', false, value + "1");
+            break;
+        case 'foreColor':
+            document.execCommand('foreColor', false, value);
+            break;
+        case 'removeFormat':
+            document.execCommand('removeFormat', false, value);
+            break;
+        case 'style':
+            document.designMode = "on";
+            document.execCommand('formatBlock', false, 'div');
+            var test = document.getSelection().focusNode.parentElement;
+            test.className = "test";
+            //$(test).addClass('test');
+            console.log(test);
+            console.log(test.classList);
+            document.designMode = "off";
+            break;
         default :
             break;
+        }
+    }
+    viewHtml() {
+        this.isHtml = !this.isHtml;
+        if(this.isHtml){
+            this.editor.nativeElement.innerText = this.editor.nativeElement.innerHTML;
+        }
+        else{
+            this.editor.nativeElement.innerHTML = this.editor.nativeElement.innerText;
         }
     }
 }
--- client/app/wysiwyg/wwtemp.module.ts
+++ client/app/wysiwyg/wwtemp.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { WwComponent } from './ww.component';
+import { SafeHtmlPipe } from './ww-safehtml.pipe';
 
 @NgModule({
     declarations : [
         WwComponent,
+        SafeHtmlPipe,
     ]
 })
 export class WwModule {
